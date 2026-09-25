3757448608efd3ecd2e7015d7f65c2daf276421e
Arturs Gorbunovs <a.gorbunovs@bilderlings.com>
2026-09-25 20:25:03 +0300

    setup route files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,23 @@
+import FavoritesPage from "./pages/FavoritesPage";
+import HistoryPage from "./pages/HistoryPage";
+import LoginPage from "./pages/LoginPage";
+import MovieDetailsPage from "./pages/MovieDetailsPage";
+import MoviesPage from "./pages/MoviesPage";
+import RegisterPage from "./pages/RegisterPage";
+import WatchlistPage from "./pages/WatchlistPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <FavoritesPage />
+      <HistoryPage />
+      <LoginPage />
+      <MovieDetailsPage />
+      <MoviesPage />
+      <RegisterPage />
+      <WatchlistPage />
+    </div>
+  );
 };
 
 export default App;
